1cc77ba5579e6d8c3c9f273e756fc93ef5428c6f
bianliao <138651187+bianliao@users.noreply.github.com>
2025-01-13 21:51:22 +0800

    js and python task

---
+++ week2/app.js
@@ -0,0 +1,20 @@
+// task01
+// build data structure
+// find the 'station' in messages
+// calculate the nearest station
+function findAndPrint(messages, currentStation){
+    // your code here
+}
+const messages={
+    "Bob":"I'm at Ximen MRT station.",
+    "Mary":"I have a drink near Jingmei MRT station.",
+    "Copper":"I just saw a concert at Taipei Arena.",
+    "Leslie":"I'm at home near Xiaobitan station.",
+    "Vivian":"I'm at Xindian station waiting for you."
+};
+findAndPrint(messages, "Wanlong"); // print Mary
+findAndPrint(messages, "Songshan"); // print Copper
+findAndPrint(messages, "Qizhang"); // print Leslie
+findAndPrint(messages, "Ximen"); // print Bob
+findAndPrint(messages, "Xindian City Hall"); // print Vivian
+
