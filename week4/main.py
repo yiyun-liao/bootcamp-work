eb425b4f690e080a926b7539ec0488f10baece91
bianliao <138651187+bianliao@users.noreply.github.com>
2025-02-05 22:01:32 +0800

    handle /signin

--- week4/main.py
+++ week4/main.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# 處理登入請求
+@app.post("/signin")
+async def signin(request:Request):
+    json_data = await request.json()
+    username = json_data.get("username","")
+    password = json_data.get("password","")
 
-app.mount("/", StaticFiles(directory="week4/service", html=True))
+    if not username or not password:
+        return {"error": "Please enter username and password"}
+    
+    if username == "test" and password == "test":
+        return {"message": "Login successful"}
+    
+    return {"error": "Username or password is not correct"}
+
+
+# 成功頁面
+# @app.get("/member")
+# async def member():
+#     return "Login successful! Welcome to the member page."
+
+# 錯誤頁面
+# @app.get("/error")
+# async def error(message: str):
+#     return f"Login failed: {message}"
+
+# from fastapi.routing import APIRoute
+# for route in app.routes:
+#     print(route.path, route.methods)
+
+app.mount("/", StaticFiles(directory="week4/service", html=True), name="static")
--- week4/service/app.js
+++ week4/service/app.js
@@ -2,12 +2,35 @@ document.addEventListener('DOMContentLoaded', function(){
     const form = document.querySelector('form');
     const checkbox = document.getElementById('agree-policy');
 
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit',async function(event){
         console.log("Submit button clicked");
         if(!checkbox.checked){
             event.preventDefault();
             alert('Please check the checkbox first');
             console.log("Checkbox is not checked! Form submission prevented.");
         }
+        event.preventDefault();
+        await signin();
     });
 })
+
+async function signin(){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try{
+        const response = await fetch("/signin",{
+            method:"POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({username, password})
+        });
+        if(!response.ok){
+            throw new Error("HTTP error! Status: ${response.status}")
+        }
+        const data = await response.json();
+        console.log(data)
+    }catch(error){
+        console.error("Error", error);
+    }
+}
+
