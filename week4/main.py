4eb8780113e88a1d26cb43ffbe8464baf85b4e6c
bianliao <138651187+bianliao@users.noreply.github.com>
2025-02-07 03:01:22 +0800

    add template

--- week4/main.py
+++ week4/main.py
@@ -1,7 +1,50 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import RedirectResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.status import HTTP_303_SEE_OTHER
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="week4/templates")
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html",{
+        "request": request,
+        "title": "歡迎光臨，請輸入帳號密碼",
+        "subtitle": "登入系統"
+    })
+
+@app.post("/signin")
+async def signin(request: Request, username: str = Form(...), password: str = Form(...)):
+    print(username, password) #"test" , "test"
+    if not username or not password:
+        return RedirectResponse(url="/error?message=Please_enter_username_and_password", status_code=HTTP_303_SEE_OTHER)
+    if username == "test" and password == "test":
+        return RedirectResponse(url="/member", status_code=HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/error?message=Username_or_password_is_not_correct", status_code=HTTP_303_SEE_OTHER)
+
+
+@app.get("/member")
+def member(request: Request):
+    return templates.TemplateResponse("member.html", {
+        "request": request,
+        "title": "歡迎光臨，這是會員頁",
+        "subtitle": "恭喜你，成功登入系統"
+        })
+
+@app.get("/error")
+def error(request: Request, message: str = "Login failed"):
+    return templates.TemplateResponse("error.html", {
+        "request": request,
+        "title": "失敗頁面",
+        "message": message
+    })
+
+app.mount("/static", StaticFiles(directory="week4/static"), name="static")
+
 
 # import time
 # from fastapi.middleware import Middleware
@@ -21,25 +64,3 @@ from starlette.status import HTTP_303_SEE_OTHER
 # ]
 
 # app = FastAPI(middleware=middleware)
-
-app = FastAPI()
-
-@app.post("/signin")
-async def signin(username: str = Form(...), password: str = Form(...)):
-    print(username, password) #"test" , "test"
-    if not username or not password:
-        return RedirectResponse(url="/error?message=Please enter username and password", status_code=HTTP_303_SEE_OTHER)
-    if username == "test" and password == "test":
-        return RedirectResponse(url="/member", status_code=HTTP_303_SEE_OTHER)
-    return RedirectResponse(url="/error?message=Username or password is not correct", status_code=HTTP_303_SEE_OTHER)
-
-@app.get("/member")
-def member():
-    return FileResponse("week4/service/member.html")
-
-@app.get("/error")
-def errot():
-    return FileResponse("week4/service/error.html")
-
-
-app.mount("/", StaticFiles(directory="week4/service", html=True), name="static")
