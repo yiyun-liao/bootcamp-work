3ba32fb811b32ee334ca2b5e0a5919f7b3a39706
bianliao <138651187+bianliao@users.noreply.github.com>
2025-02-12 13:23:43 +0800

    connect to node and python

---
+++ week5/practice/node-mysql-start.js
@@ -0,0 +1,56 @@
+const mysql=require("mysql2/promise");
+
+async function connectToMySQL(){
+    // 建立連線
+    const con = await mysql.createConnection({
+        user:"root",
+        password:"Yiyun4356",
+        host:"localhost",
+        database:"practice"
+    }); 
+    console.log('連線成功')
+
+    // INSERT INTO
+    // let [results] =  await con.execute("INSERT INTO product(name) VALUES ('綠茶')")
+    // console.log(results)
+    
+    // DELETE
+    // let [results] =  await con.execute("DELETE FROM product WHERE id=8")
+    // console.log(results)  
+    
+    // 將變數資料代入到 SQL 指令裡面
+    // UPDATE
+    // let newName='美式';
+    // let productID=1;
+    // let [results] =  await con.execute("UPDATE product SET name=? WHERE id=?", [newName,productID]);
+    // console.log(results) 
+    
+    // 取得資料
+    let [results] = await con.execute("SELECT * FROM product");
+    // console.log(results);
+
+    // 找到特定資料，1,直接印出 array 值。2, 直接在 SQL 查詢時用 WHERE id = 1。3, JS 搜尋
+    // console.log(results[0]);
+    for (let i=0; i<results.length; i++){
+        let product=results[i];
+        if (product.id === 1){
+            console.log(product.name);
+            break;
+        }
+    }
+    // // 資料個別印出 forEach()
+    // results.forEach((product)=>{
+    //     console.log(product.name)
+    // })
+    // //資料個別印出 forEach()
+    // for (let i=0; i<results.length; i++){
+    //     let product=results[i];
+    //     console.log(product.name)
+    // }
+
+
+    // 關閉連線
+    con.end()  
+}
+
+connectToMySQL();
--- week5/practice/python-mysql-start.py
+++ week5/practice/python-mysql-start.py
@@ -7,14 +7,39 @@ con=mysql.connector.connect(
     host="localhost",
     database="practice"
 )
-
 print('連線成功')
+
 # 建立 Cursor 物件，用來對資料庫下 SQL 指令
-# 將變數資料代入到 SQL 指令裡面
-productID=7
-productName="奶綠"
 cursor=con.cursor()
-cursor.execute("INSERT INTO product(id, name) VALUES (%s, %s)",(productID,productName))
-con.commit() #確定執行
+
+# 將變數資料代入到 SQL 指令裡面
+
+#INSERT INTO
+# productID=7
+# productName="奶綠"
+# cursor.execute("INSERT INTO product(id, name) VALUES (%s, %s)",(productID,productName))
+
+#UPDATE
+# productName="美式"
+# productID=1
+# cursor.execute("UPDATE product SET name=%s WHERE id=%s ", (productName, productID))
+
+# 取得多筆資料
+# cursor.execute("SELECT * FROM product")
+# data=cursor.fetchall()
+# print(data)
+# #分開取出資料
+# for row in data:
+#     print(row[0], row[1])
+
+# 取得單筆資料
+cursor.execute("SELECT * FROM product WHERE id=2")
+data=cursor.fetchone()
+print(data)
+print(data[0], data[1]) #分開取出資料
+
+#確定執行
+con.commit()
+
 # 關閉連線
 con.close()
